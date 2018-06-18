3d167454df054b376ecece55a43431f0c8285696
Jakub Minarik <minarik@mobilesoft.eu>
2018-06-18 12:11:12 +0200

    about activitka

--- app/src/main/java/com/jakubminarik/dashcam/about/AboutActivity.java
+++ app/src/main/java/com/jakubminarik/dashcam/about/AboutActivity.java
@@ -1,13 +1,20 @@
 package com.jakubminarik.dashcam.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.jakubminarik.dashcam.R;
 import com.jakubminarik.dashcam.base.BaseActivityDI;
 import com.jakubminarik.dashcam.base.BasePresenter;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import dagger.android.AndroidInjection;
@@ -15,6 +22,9 @@ import dagger.android.AndroidInjection;
 public class AboutActivity extends BaseActivityDI implements AboutActivityView {
     @Inject
     AboutActivityPresenter presenter;
+
+    @BindView(R.id.appLogoImageView)
+    ImageView appLogoImageView;
 
     @Override
     public BasePresenter getPresenter() {
@@ -27,10 +37,38 @@ public class AboutActivity extends BaseActivityDI implements AboutActivityView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
+
+
+        getSupportActionBar().setTitle(R.string.about_dashcam);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Glide.with(this).load(R.drawable.logo).into(appLogoImageView);
     }
 
-    @OnClick(R.id.aboutButton)
-    public void aboutButtonClicked() {
-        presenter.aboutButtonClicked();
+    @OnClick(R.id.contactDevButton)
+    void onContactDevButtonClicked() {
+        String[] TO = {"jakub.minarik.11@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "DashCam feedback");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Email developer..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(AboutActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
